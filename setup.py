7d643c5ee84f8382c2e2bf0d7aa1c6c056aea8e2
Tobi <txhx38@gmail.com>
2021-07-07 02:23:57 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import setuptools
 
+
 # VERSION
 with open('src/lofile/core/__init__.py') as file:
     for line in file.readlines():
@@ -9,21 +10,26 @@ with open('src/lofile/core/__init__.py') as file:
             __version__: str
             exec(line)
 
+
 # SETUP
 setuptools.setup(
+
     name = 'lofile-lool',
     version = __version__,
     author = 'lool',
     author_email = 'txhx@gmail.com',
     description = 'The lofile package!',
     url = 'https://github.com/user3838',
+
     package_dir = {'': 'src'},
     packages = setuptools.find_packages('src'),
     python_requires = '>= 3.8',
     install_requires = ['pycryptodome'],
+
     entry_points = {
         'console_scripts': [
             'lofile = lofile.cli:main'
         ]
     }
+
 )
